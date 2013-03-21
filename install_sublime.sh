2b1ec8a5dbeeb8e978a577ecf91227aaf9a5c83a
Tyler Menezes <tylermenezes@gmail.com>
2013-03-21 22:34:07 +0000

    Add .desktop file for Sublime

--- install_sublime.sh
+++ install_sublime.sh
@@ -21,3 +21,30 @@ rm *
 git init
 git remote add origin git@github.com:tylermenezes/Sublime-Text-Settings.git
 git pull origin master
+
+
+
+cat << 'EOF' > "~/.local/share/applications/Sublime Text 2.desktop"
+#!/usr/bin/env xdg-open
+
+[Desktop Entry]
+Version=1.0
+Name=Sublime Text 2
+Comment=Edit text files using Sublime Text 2
+Exec=subl %u
+Terminal=false
+Icon="/opt/sublime_text/Icon/48x48/sublime_text.png"
+Type=Application
+Categories=TextEditor;IDE;Development;Application;Utility
+X-Ayatana-Desktop-Shortcuts=NewWindow
+Icon[en_US]=/opt/sublime_text/Icon/128x128/sublime_text.png
+Encoding=UTF-8
+StartupNotify=true
+MimeType=text/plain;
+
+
+[NewWindow Shortcut Group]
+Name=New Window
+Exec=subl -n
+TargetEnvironment=Unity
+EOF;
